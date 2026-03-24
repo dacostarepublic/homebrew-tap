a2b06ec9e0c82ab75be085d88c4e12e88c88f732
dacostarepublic <conal@dacostarepublic.co.za>
2026-03-24 14:38:52 +0200

    Update lumen formula to 2.0.0

--- Formula/lumen.rb
+++ Formula/lumen.rb
@@ -1,9 +1,9 @@
 class Lumen < Formula
   desc "Terminal-first wallpaper manager for macOS"
   homepage "https://github.com/dacostarepublic/lumen"
-  version "1.1.0"
-  url "https://github.com/dacostarepublic/lumen/releases/download/v1.1.0/lumen-v1.1.0-macos.tar.gz"
-  sha256 "b8f68df558306d505cdfbeedcc451ac9ae2eb5d20ff8bb557638de7c23407fca"
+  version "2.0.0"
+  url "https://github.com/dacostarepublic/lumen/releases/download/v2.0.0/lumen-v2.0.0-macos.tar.gz"
+  sha256 "bfe9f632c56890eb796152f5cfaf6f08ec5096a4df16a4014c0c860a5797e946"
   license "MIT"
 
   depends_on macos: :ventura
